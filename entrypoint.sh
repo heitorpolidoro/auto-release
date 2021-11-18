678fd9e906ccf68305ec56a3d0877c40057b0bb7
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-18 20:44:39 -0300

    getting user 5

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
-pwd
-ls -a /github/workspace/.github
-ls -a /github/home
-ls -a /github
 
 sleep 10
 echo "::group::GitHub authentication"
 echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
 echo "::endgroup::"
-
+set -x
+pwd
+ls -a /github/workspace/.github
+ls -a /github/home
+ls -a /github
 echo "::group::Create Release"
 echo "Estou no entrypoint"
 echo "::endgroup::"
