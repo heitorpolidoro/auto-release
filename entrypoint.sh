82d1696f25a1536c1f9ab4a77efa92cd5a2dd24c
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-18 20:57:33 -0300

    allowed users

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,10 @@ GITHUB_USER=$(gh auth status 2>&1 | grep Logged | sed 's/.* as \(.*\) (.*/\1/')
 echo "Logged as $GITHUB_USER"
 echo "::endgroup::"
 
+if [[ -n "$INPUT_ALLOWED_USERS" ]] && [[ ! " ${INPUT_ALLOWED_USERS[*]} " =~ " ${GITHUB_USER} " ]]
+then
+  echo 1
+fi
 echo "::group::Create Release"
 echo "Estou no entrypoint"
 echo "::endgroup::"
