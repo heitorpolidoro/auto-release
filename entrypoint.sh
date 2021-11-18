301d4f0940db1090b0629a3473813ad688bc2cf3
Heitor Polidoro <heitor.polidoro@gmail.com[color]>
2021-11-18 19:54:46 -0300

    gh login

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-echo "::group::GRUPO"
+echo "::group::Create Release"
 set -e
 
+echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
+gh auth status
 echo "Estou no entrypoint"
 echo "::endgroup::"
 
