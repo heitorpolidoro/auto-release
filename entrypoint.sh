ba3f4f408554e37c43d0faefed874a62cd55282d
Heitor Polidoro <heitor.polidoro@gmail.com[color]>
2021-11-18 20:23:26 -0300

    input typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 echo "::group::GitHub authentication"
-echo "$INPUT_PERSONAL-ACCESS-TOKEN"
-echo "$INPUT_PERSONAL-ACCESS-TOKEN" | gh auth login --with-token
+echo "$INPUT_PERSONAL_ACCESS_TOKEN"
+echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
 gh auth status
 echo "::endgroup::"
 
