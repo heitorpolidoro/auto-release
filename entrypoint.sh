0eea673bf952212194267bb1d5847b4dd2705adb
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-18 20:51:14 -0300

    getting user 7

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -e
 
-sleep 10
 echo "::group::GitHub authentication"
 echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
+GITHUB_USER=$(gh auth status 2>&1 | grep Logged | sed 's/.* as \(.*\) (.*/\1/')
+echo "Logged as $GITHUB_USER"
 echo "::endgroup::"
-set -x
-pwd
-ls -a /github/home
-cat /github/home/.config/gh/hosts.yml
+
 echo "::group::Create Release"
 echo "Estou no entrypoint"
 echo "::endgroup::"
