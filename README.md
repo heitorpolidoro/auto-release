ee150a141a5aa7cda65491dd56069686975e052b
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-11-19 11:33:21 -0300

    vzero

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# auto-release
+# Autorelease GitHub Action
 Action to create a release automatically
+
+### Usage
+```yaml
+    steps:
+      - uses: actions/checkout@v2
+
+      - name: Create GitHub Release
+        uses: heitorpolidoro/auto-release@v0
+        with:
+          personal_access_token: ${{ secrets.PAT }}
+          allowed_users: heitorpolidoro
+          version_file: version.txt
+```
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,31 @@
 #!/bin/bash
+BOLD='\x1b[1m'
+NORMAL="\x1b[0m"
 set -e
-echo "$GITHUB_ACTOR"
-echo "::group::GitHub authentication"
-echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
-GITHUB_USER=$(gh auth status 2>&1 | grep Logged | sed 's/.* as \(.*\) (.*/\1/')
-echo "Logged as $GITHUB_USER"
-echo "::endgroup::"
-
-if [[ -n "$INPUT_ALLOWED_USERS" ]] && [[ ! " ${INPUT_ALLOWED_USERS[*]} " =~ " ${GITHUB_USER} " ]]
+if [[ -n "$INPUT_ALLOWED_USERS" ]] && [[ ! " ${INPUT_ALLOWED_USERS[*]} " =~ " ${GITHUB_ACTOR} " ]]
 then
-  echo nao pode
+  echo -e "User $BOLD$GITHUB_ACTOR$NORMAL is not allowed to auto-release"
 else
-  echo pode
+  echo "::group::GitHub authentication"
+    echo "$INPUT_PERSONAL_ACCESS_TOKEN" | gh auth login --with-token
+  echo "::endgroup::"
+
+  NEW_RELEASE=$(cat "$INPUT_VERSION_FILE")
+  echo "::group::Creating Release v$NEW_RELEASE"
+    git fetch --prune --tags --quiet
+#    git fetch --prune --unshallow --tags --quiet
+    LAST_RELEASE=$(git tag --list | sort | tail -1)
+    if [[ -n "$LAST_RELEASE" ]]
+    then
+      echo "Last Release: $LAST_RELEASE"
+      if [[ "$LAST_RELEASE" == "v$NEW_RELEASE" ]]
+      then
+        >&2 echo -e "Version$BOLD v$NEW_RELEASE$NORMAL already exists"
+        exit 1
+      fi
+    fi
+
+    gh release create "v$NEW_RELEASE"
+  echo "::endgroup::"
 fi
-echo "::group::Create Release"
-echo "Estou no entrypoint"
-echo "::endgroup::"
 
