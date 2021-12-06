fa04abda0c1464f4b78084cfe70f95c95b29b45d
Heitor Polidoro <heitor.polidoro@gmail.com>
2021-12-06 15:00:00 -0300

    fix(version-in-module): install requirements quietly

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ remove_release()
 get_new_release_version()
 {
   if [[ "$INPUT_VERSION_FILE" == *__init__.py ]]; then
-    if [ -f requirements.txt ]; then pip install -r requirements.txt; fi
+    if [ -f requirements.txt ]; then pip install -r requirements.txt -q; fi
     module=${INPUT_VERSION_FILE/\/__init__.py/}
     python3 -c "import ${module};print(${module}.VERSION)"
   else
